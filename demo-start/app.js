5b67aa79650ebd8d6025fc08d9ff19f6fdf0d44a
virtualoverride <b.yajra.eroble@gmail.com>
2015-05-29 15:25:37 +0800

    add dev branch separate the file for helloWorld

--- demo-start/app.js
+++ demo-start/app.js
@@ -1,23 +1,7 @@
 (function(){
 	'use strict';
-	
-	var HelloWorld = function(helloWorldService){
-		this.hello = helloWorldService.hello();
-	};
-
-	HelloWorld.annotations = [
-		new angular.ComponentAnnotation({
-			selector: 'hello',
-			appInjector: [Service.HelloWorld]
-		}),
-		new angular.ViewAnnotation({
-			template: '{{ hello }}'
-		})
-	];
-
-	HelloWorld.parameters = [[Service.HelloWorld]];
 
 	document.addEventListener('DOMContentLoaded', function() {
-	  angular.bootstrap(HelloWorld);
+	  angular.bootstrap(App.HelloWorld);
 	});
 })();
